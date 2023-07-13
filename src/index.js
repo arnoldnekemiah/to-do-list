fa3a10d1c8290f63f97a0961695fbdb4c7fbd18c
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-07-13 09:43:37 +0300

    made list structure

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
-import _ from 'lodash';
-import printMe from './print.js';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+// Array object of tasks
+const tasks = [
+  {
+    description: 'Play Chess',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Sleep',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Code',
+    completed: false,
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  btn.innerHTML = 'Click me and check the console!';
+const rendertasks = () => {
+  const taskList = document.getElementById('task-container');
+  taskList.innerHTML = '';
+  // sort tasks by index
+  tasks.sort((a, b) => a.index - b.index);
 
-  btn.onclick = printMe;
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.classList = 'listItmes';
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    const span = document.createElement('span');
+    span.textContent = task.description;
+    li.appendChild(check);
+    li.appendChild(span);
+    const ellipsis = document.createElement('i');
+    ellipsis.classList.add('fa', 'fa-ellipsis-v');
+    ellipsis.setAttribute('aria-hidden', 'true');
+    li.appendChild(ellipsis);
+    taskList.appendChild(li);
+  });
+};
 
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener('load', rendertasks);
